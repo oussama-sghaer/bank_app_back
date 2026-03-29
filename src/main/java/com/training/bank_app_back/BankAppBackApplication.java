5bb3ec8d8000ea686cfeede785c21f5c0f7a36b5
oussama-sghaier-perfanalysis <oussama.sghaier@perfanalysis.consulting>
2026-03-29 21:14:24 +0100

    init

---
+++ src/main/java/com/training/bank_app_back/BankAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.training.bank_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/bank_app_back/BankAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.bank_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
