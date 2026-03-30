1ee9733f9eacc16432e388a61e7aab891ca68dff
oussama-sghaier-perfanalysis <oussama.sghaier@perfanalysis.consulting>
2026-03-30 20:46:16 +0100

    fix(core):fix account controller request mapping

--- src/main/java/com/training/bank_app_back/core/controllers/v1/AccountController.java
+++ src/main/java/com/training/bank_app_back/core/controllers/v1/AccountController.java
@@ -5,11 +5,13 @@ import com.training.bank_app_back.core.entities.Account;
 import com.training.bank_app_back.core.services.v1.interfaces.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("api/v1/accounts")
+@RestController()
+@RequestMapping("/api/v1/accounts")
 public class AccountController {
     private final AccountService accountService;
     public AccountController(AccountService accountService) {
